9c48556c00102f702760dd509084ebbe9040c7bc
André <imandreleandro@gmail.com>
2025-02-19 17:15:25 +0000

    Added comment to Main method

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -4,6 +4,10 @@ namespace MeuPrograma
 {
     public class Program
     {
+        /// <summary>
+        /// Say Hello and bye to things.
+        /// </summary>
+        /// <param name="args">Ignore this</param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
