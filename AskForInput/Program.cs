2e755f6e988e982b61fd84250dfe086d9f89c0dd
André <imandreleandro@gmail.com>
2025-02-19 18:21:27 +0000

    A soma de 2 números digitados pelo utilizador

---
+++ AskForInput/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um número inteiro:");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+            Console.WriteLine("Insira um número real:");
+            input = Console.ReadLine();
+            float number2 = float.Parse(input);
+
+            Console.WriteLine($"A soma de {number} e {number2} é: {number+number2}");
+        }
+    }
+}
