d0b13adae94ced7a194fd1b612714d10e970eaa6
André <imandreleandro@gmail.com>
2025-02-19 18:14:41 +0000

    Added many more strings

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -6,6 +6,9 @@ namespace ManyStrings
     {
         private static void Main(string[] args)
         {
+            double xx = 1.23456;
+            int ii = 19;
+
             string welcome_text = "Hello and welcome to this world!";
             string player_name = "John Johnson";
             char c = '\u2622';
@@ -17,6 +20,13 @@ namespace ManyStrings
             int xp = 500;
             int xp_needed = 1000;
             Console.WriteLine($"O XP necessário é {xp_needed-xp} ({xp_needed} - {xp})");
+        
+            Console.WriteLine($"2 casas: {xx:f2}");
+            Console.WriteLine($"Percentagem 1 casa: {xx:p1}");
+
+            Console.WriteLine($"\nHexadecimal: {ii:x}");
+            Console.WriteLine($"\nMoeda: {ii:c}");
+
         }
     }
 }
