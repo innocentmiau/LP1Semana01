277bc066eb9fb076f6f3f87e6cc3187a72557c55
André <imandreleandro@gmail.com>
2025-02-19 17:53:22 +0000

    Added different strings text

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string welcome_text = "Hello and welcome to this world!";
+            string player_name = "John Johnson";
+            char c = '\u2622';
+
+            string formatted_text = $"{welcome_text}\n\n> Player Name: {player_name} (NEW)\t{c}";
+        
+            Console.WriteLine(formatted_text);
+        }
+    }
+}
