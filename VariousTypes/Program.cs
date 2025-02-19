b547bda29a70d558b4862ae5703e92929d22da6c
André <imandreleandro@gmail.com>
2025-02-19 17:33:30 +0000

    Added unicode chars

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -10,6 +10,13 @@ namespace VariousTypes
             int xp = 2500000;
             long xp_needed = 40000000000;
             Console.WriteLine($"Lvl. {level}: {xp}/{xp_needed}");
+
+            char[] cs = {'\u270D', '\u27A8', '\u260E', '\u2705'};
+            string chars = "";
+            foreach (char c in cs) {
+                chars += $" {c} /";
+            }
+            Console.WriteLine($"\nChars Unicode:{chars.Substring(0, chars.Length-1)}");
         }
     }
 }
