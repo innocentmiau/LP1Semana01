9b41460823cc095e2f383ec601b1997017e810cc
André <imandreleandro@gmail.com>
2025-02-19 17:21:22 +0000

    added Various types

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            short level = 5;
+            int xp = 2500000;
+            long xp_needed = 40000000000;
+            Console.WriteLine($"Lvl. {level}: {xp}/{xp_needed}");
+        }
+    }
+}
