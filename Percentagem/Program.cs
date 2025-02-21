1bd5337584d0050aaead18ae74923d9cf19ee34b
André <imandreleandro@gmail.com>
2025-02-21 14:09:18 +0000

    Added a test to showcase numbers between 0 and 1 and their corresponding percentagens

---
+++ Percentagem/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Percentagem
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Insira 4 números reais entre 0 e 1:");
+                try
+                {
+                    float number1 = float.Parse(Console.ReadLine());
+                    float number2 = float.Parse(Console.ReadLine());
+                    float number3 = float.Parse(Console.ReadLine());
+                    float number4 = float.Parse(Console.ReadLine());
+                    if (number1 < 0 || number1 > 1 
+                                    || number2 < 0 || number2 > 1
+                                    || number3 < 0 || number3 > 1 
+                                    || number4 < 0 || number4 > 1)
+                    {
+                        Console.WriteLine("Inseriu algum número real que não são entre 0 e 1.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{number1,7:p2}");
+                        Console.WriteLine($"{number2,7:p2}");
+                        Console.WriteLine($"{number3,7:p2}");
+                        Console.WriteLine($"{number4,7:p2}");
+                        break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Inseriu conteúdo que não é um número real.");
+                }
+            }
+            
+            
+        }
+    }
+}
