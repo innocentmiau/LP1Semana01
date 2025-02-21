088cd7ff6b9debab22abe1d5b9287c4ffe5262b1
André <imandreleandro@gmail.com>
2025-02-21 13:11:27 +0000

    Changed to accept only a int and real number in it's respective inputs

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -7,20 +7,27 @@ namespace AskForInput
         private static void Main(string[] args)
         {
             Console.WriteLine("Insira um número inteiro:");
-            string input = Console.ReadLine();
+            string input;
             int number = 0;
-            try {
-                number = int.Parse(input);
-            } catch (Exception e) {
-                Console.WriteLine("Inseriu algo que não era um número inteiro, o valor 0 foi atribuido.");
+            while (true) {
+                input = Console.ReadLine();
+                try {
+                    number = int.Parse(input);
+                    break;
+                } catch (Exception e) {
+                    Console.WriteLine("Inseriu algo que não era um número inteiro, insira algo correto!");
+                }
             }
             Console.WriteLine("Insira um número real:");
-            input = Console.ReadLine();
             float number2 = 0f;
-            try {
-                number2 = float.Parse(input);
-            } catch (Exception e) {
-                Console.WriteLine("Inseriu algo que não era um número real, o valor 0 foi atribuido.");
+            while (true) {
+                input = Console.ReadLine();
+                try {
+                    number2 = float.Parse(input);
+                    break;
+                } catch (Exception e) {
+                    Console.WriteLine("Inseriu algo que não era um número real, o valor 0 foi atribuido.");
+                }
             }
 
             Console.WriteLine($"A soma de {number} e {number2} é: {number+number2}");
